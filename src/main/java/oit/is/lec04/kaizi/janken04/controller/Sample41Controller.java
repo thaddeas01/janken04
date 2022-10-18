68a7695fa3fdeb8997ecffa27220b0a765b10933
e1b20013 <e1b20013@st.oit.ac.jp>
2022-10-18 15:21:41 +0900

    sample4-3

--- src/main/java/oit/is/lec04/kaizi/janken04/controller/Sample41Controller.java
+++ src/main/java/oit/is/lec04/kaizi/janken04/controller/Sample41Controller.java
@@ -30,6 +30,11 @@ public class Sample41Controller {
     return "sample41.html";
   }
 
+  @GetMapping("step3")
+  public String sample43() {
+    return "sample43.html";
+  }
+
   @GetMapping("step2/{id}")
   public String sample42(@PathVariable Integer id, ModelMap model) {
     Chamber chamber2 = chamberMapper.selectById(id);
