dc5f340222682c4e624c4f181c421206bd1fa518
e1b20013 <e1b20013@st.oit.ac.jp>
2022-10-18 15:01:14 +0900

    sample4-1

---
+++ src/main/java/oit/is/lec04/kaizi/janken04/controller/Sample41Controller.java
@@ -0,0 +1,24 @@
+package oit.is.lec04.kaizi.janken04.controller;
+
+import java.security.Principal;
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/sample4")
+public class Sample41Controller {
+
+  @GetMapping("step1")
+  public String sample41() {
+    return "sample41.html";
+  }
+}
