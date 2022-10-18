f590a75b732eb6c821b9cde9fe04eb1e0d5edb28
thaddeas01 <shemthaddeas01@gmail.com>
2022-10-18 14:12:31 +0900

    初期化

---
+++ src/main/java/oit/is/lec04/kaizi/janken04/Janken04Application.java
@@ -0,0 +1,13 @@
+package oit.is.lec04.kaizi.janken04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Janken04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Janken04Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/lec04/kaizi/janken04/Janken04ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.lec04.kaizi.janken04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Janken04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
