b99e380126dcabd494144d22b69833e2f7947f18
e1b20013 <e1b20013@st.oit.ac.jp>
2022-10-18 15:18:31 +0900

    sample4-2

--- src/main/java/oit/is/lec04/kaizi/janken04/controller/Sample41Controller.java
+++ src/main/java/oit/is/lec04/kaizi/janken04/controller/Sample41Controller.java
@@ -13,12 +13,28 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import oit.is.lec04.kaizi.janken04.model.Chamber;
+import oit.is.lec04.kaizi.janken04.model.ChamberMapper;
+//import oit.is..model.ChamberUser;
+//import oit.is.inudaisuki.springboot_samples.model.UserInfo;
+
 @Controller
 @RequestMapping("/sample4")
 public class Sample41Controller {
 
+  @Autowired
+  ChamberMapper chamberMapper;
+
   @GetMapping("step1")
   public String sample41() {
+    return "sample41.html";
+  }
+
+  @GetMapping("step2/{id}")
+  public String sample42(@PathVariable Integer id, ModelMap model) {
+    Chamber chamber2 = chamberMapper.selectById(id);
+    model.addAttribute("chamber2", chamber2);
+
     return "sample41.html";
   }
 }
---
+++ src/main/java/oit/is/lec04/kaizi/janken04/model/ChamberMapper.java
@@ -0,0 +1,16 @@
+package oit.is.lec04.kaizi.janken04.model;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface ChamberMapper {
+
+  @Select("SELECT id,userName,chamberName from chamber where id = #{id}")
+  Chamber selectById(int id);
+
+}
